1cfcbe544ce5695ab383b634029c12e30c9be89a
pouya-shekari <pshekari37@gmail.com>
2022-03-02 13:44:51 +0330

    fix responsive

--- CW/controller.js
+++ CW/controller.js
@@ -1,7 +1,7 @@
 function addCard({firstName , email , avatar , lastName}){
-    let cardHtml = `<div class="col-3 m-2">
+    let cardHtml = `<div class="col-12 col-xl-3 col-lg-4 col-sm-6 m-2">
     <div class="w-100 h-100 box p-3 d-flex">
-        <div class="rounded" style="width: 150px;">
+        <div class="rounded" style="width: 10rem;">
             <img style="width:100% ; height: 100%;" class="rounded" src="${avatar}">
         </div>
         <div class="d-flex flex-column pl-3 justify-content-center">
